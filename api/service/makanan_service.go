00a22bc789ad7c8aa32cffe0c0a45e61d4761ee3
Saadyyyy <laodesaady12345@gmail.com>
2024-02-13 12:59:00 +0700

    fix bug service for unit testing

--- api/service/makanan_service.go
+++ api/service/makanan_service.go
@@ -3,12 +3,11 @@ package service
 import (
 	"github.com/Saadyyyy/Unit-Testing/api/repository"
 	"github.com/Saadyyyy/Unit-Testing/models"
-	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 )
 
 type MakananService interface {
-	Create(ctx *gin.Context) (*models.Makanan, error)
+	Create(input models.Makanan) (*models.Makanan, error)
 }
 
 type MakananServiceImpl struct {
@@ -19,18 +18,10 @@ func NewMakananService(repo repository.MakananRepository) MakananService {
 	return &MakananServiceImpl{repo: repo}
 }
 
-func (us *MakananServiceImpl) Create(ctx *gin.Context) (*models.Makanan, error) {
-	input := models.Makanan{}
-
-	if err := ctx.ShouldBindJSON(&input); err != nil {
-		return nil, err
-	}
+func (us *MakananServiceImpl) Create(input models.Makanan) (*models.Makanan, error) {
 
 	validate := validator.New()
-	err := validate.Struct(input)
-	if err != nil {
-		return nil, err
-	}
+	validate.Struct(input)
 
 	result, err := us.repo.Create(input)
 	if err != nil {
